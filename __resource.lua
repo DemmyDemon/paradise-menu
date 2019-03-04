e88cd8a94f8be2a8ae4a5ebbdb47bc49bb8960d9
Fredrik Vold <fredrik@webkonsept.com>
2019-03-04 12:04:00 +0100

    Why the obsolete RMV?

--- __resource.lua
+++ __resource.lua
@@ -1,4 +1,4 @@
-resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 description 'Menu in the style of Swedish Paradise'
 
 client_scripts {'menu.lua','example.lua'}
